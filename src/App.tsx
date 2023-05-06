0c48b6bdbfa11c833da7a8320784a36fc6a71e68
ianK-code <kian99564@gmail.com>
2023-05-06 23:08:54 +0300

    60% Done

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,35 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import InputField from "./components/InputField.js";
+// import TodoList from "./components/TodoList.js";
+import { Todo } from "./Model.js";
 
 function App() {
   const [todo, setTodo] = useState<string>("");
 
-  console.log(todo)
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  // Adds every todo to the todoArray
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (todo) {
+      setTodos([{ id: Date.now(), todo, isDone: false }, ...todos]);
+
+      setTodo("");
+    }
+  };
+
   return (
     <div className="App">
       <span className="heading">Taskify</span>
-      <InputField todo={todo} setTodo={setTodo}/>
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+
+      {/* <TodoList /> */}
+
+      {todos.map((t, index) => (
+        <li key={index}>{t.todo}</li>
+      ))}
     </div>
   );
 }
