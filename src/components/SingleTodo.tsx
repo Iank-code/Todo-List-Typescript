2b3e104241ee7c20154f09e807b9e29f6853ce05
ianK-code <kian99564@gmail.com>
2023-05-08 23:07:16 +0300

    Mark As Done implemented Successfuly

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { Todo } from "../Model.js";
-// import { FiEdit } from "react-icons/fi";
+import { FiEdit } from "react-icons/fi";
+import { AiFillDelete } from "react-icons/ai";
+import { IoMdDoneAll } from "react-icons/io";
+import "./styles.css";
 
 type Props = {
   todo: Todo;
@@ -9,16 +12,32 @@ type Props = {
 };
 
 const SingleTodo = ({ todo, todos, setTodos }: Props) => {
-  console.log(todos);
+  const handleDone = (id: number) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, isDone: !todo.isDone } : todo
+      )
+    );
+  };
 
   return (
-    <form className="todos_single" onSubmit={() => setTodos([])}>
-      <span className="todos_single_text">{todo.todo}</span>
+    <form className="todos_single">
+      {todo.isDone ? (
+        <s className="todos_single_text">{todo.todo}</s>
+      ) : (
+        <span className="todos_single_text">{todo.todo}</span>
+      )}
 
       <div>
-        <span className="icon">{/* <FiEdit /> */}</span>
-        <span className="icon"></span>
-        <span className="icon"></span>
+        <span className="icon">
+          <FiEdit />
+        </span>
+        <span className="icon">
+          <AiFillDelete />
+        </span>
+        <span className="icon" onClick={() => handleDone(todo.id)}>
+          <IoMdDoneAll />
+        </span>
       </div>
     </form>
   );
